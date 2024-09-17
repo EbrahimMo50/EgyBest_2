55694a8296802c97c7c0bf7284ef568fe3a257d2
Ebrahim Mohsen <ibrahimjarb69@gmail.com>
2024-09-17 09:58:12 +0300

    init app
    this is the first commit of the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavComponent,FooterComponent,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
