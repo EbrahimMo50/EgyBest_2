24354fc7ae828d13462c02e8136b497f7351f93d
Ebrahim Mohsen <ibrahimjarb69@gmail.com>
2024-09-18 14:48:09 +0300

    finished logging in logic

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -15,4 +15,8 @@ export class AuthService {
   Register(FormObject:any) : Observable<any>{
     return this._httpClient.post("https://localhost:7181/api/Auth/Register",FormObject.value);
   }
+
+  Login(FormObject:any) : Observable<any>{
+    return this._httpClient.post("https://localhost:7181/api/Auth/Login?email=E%40gmail.com&password=1234",FormObject.value ,{ responseType: 'text' });
+  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  constructor(private  _authService:AuthService){ }
+
+  FormGroup:FormGroup = new FormGroup({
+    email:new FormControl(null,[Validators.required]),
+    password:new FormControl(null,[Validators.required])
+  });
+
+  Login(FormGroup:FormGroup){
+
+    this._authService.Login(FormGroup).subscribe((data)=>{
+      console.log(data);
+    });
+    
+  }
 }
